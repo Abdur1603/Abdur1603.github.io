463c4c5b1ace2a9985304dca24311bd7265db185
Abdur1603 <152826572+Abdur1603@users.noreply.github.com>
2024-10-09 22:20:17 +0800

    Add files via upload

---
+++ Posttest5/pelanggan.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 09 Okt 2024 pada 16.19
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pelanggan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `plgn`
+--
+-- Kesalahan membaca struktur untuk tabel pelanggan.plgn: #1932 - Table &#039;pelanggan.plgn&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel pelanggan.plgn: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pelanggan`.`plgn`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
